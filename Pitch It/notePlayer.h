17b929c8caff4de318bf2d366752bebee328f4ec
Heron Yang <v6510young88@gmail.com>
2013-09-17 01:10:39 +0800

    heron's initial commit

---
+++ Pitch It/notePlayer.h
@@ -0,0 +1,15 @@
+//
+//  notePlayer.h
+//  Note Player
+//
+//  Created by heron on 9/11/13.
+//  Copyright (c) 2013 heron. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface notePlayer : NSObject
+
+- (void)playNote:(NSInteger)noteNumber;
+
+@end
---
+++ Pitch It/notePlayer.m
@@ -0,0 +1,71 @@
+//
+//  notePlayer.m
+//  Note Player
+//
+//  Created by heron on 9/11/13.
+//  Copyright (c) 2013 heron. All rights reserved.
+//
+
+#import "notePlayer.h"
+#import <AVFoundation/AVFoundation.h>
+
+#define VOLUME 1.0f
+#define NOTE_LOWER_BOUND	1
+#define NOTE_UPPER_BOUND	88
+
+@interface notePlayer ()
+@property (strong, nonatomic)AVAudioPlayer *player;
+@end
+
+@implementation notePlayer
+
+// constructor
+- (id)init {
+	self = [super init];
+	if (self) {
+		NSLog(@"[Note Player] note player is loaded.");
+	}
+	return self;
+}
+
+- (AVAudioPlayer *)player {
+	if (_player == nil) {
+		NSString *mp3Path = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"40.mp3"];
+		NSURL *mp3Url = [NSURL fileURLWithPath:mp3Path];
+		
+		NSError *err;
+		_player = [[AVAudioPlayer alloc] initWithContentsOfURL:mp3Url error:&err];
+		_player.volume = 1.0f;
+		_player.numberOfLoops = 0;
+		[_player prepareToPlay];
+	}
+	return _player;
+}
+
+- (void)playerResetToNote:(NSString *)audioFilename{
+	NSString	*mp3Path = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:audioFilename];
+	NSURL	*mp3Url = [NSURL fileURLWithPath:mp3Path];
+	NSError	*err;
+	
+	self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:mp3Url error:&err];
+	self.player.volume = 1.0f;
+	self.player.numberOfLoops = 0;
+	[self.player prepareToPlay];
+}
+
+- (void)playNote:(NSInteger)noteNumber {
+	// check input valid
+	if (noteNumber < NOTE_LOWER_BOUND || noteNumber > NOTE_UPPER_BOUND) {
+		NSLog(@"[Note Player] input range error");
+	}
+	
+	// setup player
+	NSString *audioFilename = [[NSString alloc] initWithFormat:@"%02d.mp3", noteNumber];
+	NSLog(@"[Note Player] %@ is played", audioFilename);
+	[self playerResetToNote:audioFilename];
+	
+	// play
+	[self.player play];
+}
+
+@end
