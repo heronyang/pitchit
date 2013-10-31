49e7beba0777922f5c002b44558f1d21e7e2e0f2
Heron Yang <v6510young88@gmail.com>
2013-11-01 03:03:33 +0800

    almost there 2

---
+++ initStartViewController.h
@@ -0,0 +1,13 @@
+//
+//  initStartViewController.h
+//  Pitch It
+//
+//  Created by heron on 10/31/13.
+//  Copyright (c) 2013 heron & minnie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface initStartViewController : UIViewController
+
+@end
---
+++ initStartViewController.m
@@ -0,0 +1,17 @@
+//
+//  initStartViewController.m
+//  Pitch It
+//
+//  Created by heron on 10/31/13.
+//  Copyright (c) 2013 heron & minnie. All rights reserved.
+//
+
+#import "initStartViewController.h"
+
+@implementation initStartViewController
+
+- (void)viewDidLoad{
+	[self performSegueWithIdentifier:@"initStart" sender:self];
+}
+@end
+
